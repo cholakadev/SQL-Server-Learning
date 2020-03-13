1096b691b50348a41c5facf552b21fca126a0fdd
cholakovge <cholakovge@gmail.com>
2020-03-13 22:07:28 +0200

    New Querry

--- Build-in Functions/build-infuncs.sql
+++ Build-in Functions/build-infuncs.sql
@@ -89,3 +89,23 @@ LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)) AS Mix
 FROM Peaks AS p
 INNER JOIN Rivers AS r ON RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
 ORDER BY Mix
+
+-- (11) Find all games with part of the day and duration sorted by game name alphabetically then by duration (alphabetically, 
+-- not by the timespan) and part of the day (all ascending). Parts of the day should be Morning (time is >= 0 and < 12), 
+-- Afternoon (time is >= 12 and < 18), Evening (time is >= 18 and < 24). Duration should be Extra Short (smaller or equal to 3), 
+-- Short (between 4 and 6 including), Long (greater than 6) and Extra Long (without duration). 
+
+SELECT [Name] AS [Game],
+  CASE
+  WHEN DATEPART(HOUR, [Start]) BETWEEN 0 AND 11 THEN 'Morning'
+  WHEN DATEPART(HOUR, [Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+  ELSE 'Evening'
+  END AS [Part of the Day],
+  CASE
+  WHEN Duration <= 3 THEN 'Extra Short'
+  WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+  WHEN Duration > 6 THEN 'Long'
+  WHEN Duration IS NULL THEN 'Extra Long'
+  END AS [Duration]
+  FROM Games
+  ORDER BY [Game], [Duration], [Part of the Day]
