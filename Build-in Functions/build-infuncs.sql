77ddd730fc7039196fc6bc6bb180f970d141cd58
cholakovge <cholakovge@gmail.com>
2020-03-13 21:51:55 +0200

    New task

--- Build-in Functions/build-infuncs.sql
+++ Build-in Functions/build-infuncs.sql
@@ -83,3 +83,9 @@ LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)) AS Mix
 FROM Peaks AS p, Rivers AS r
 WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
 ORDER BY [Mix]
+
+SELECT p.PeakName, r.RiverName,
+LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)) AS Mix
+FROM Peaks AS p
+INNER JOIN Rivers AS r ON RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY Mix
