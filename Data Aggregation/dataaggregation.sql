2c1c20715a41ad5d62fe591544dd2626a010015e
cholakovge <cholakovge@gmail.com>
2020-03-14 12:37:45 +0200

    New querry

--- Data Aggregation/dataaggregation.sql
+++ Data Aggregation/dataaggregation.sql
@@ -28,6 +28,12 @@ GROUP BY DepositGroup
 
 -- (5) Select all deposit groups and their total deposit sums but only for the wizards who have their magic wands crafted by Ollivander family.
 
+SELECT DepositGroup,
+SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+
 -- (6) Select all deposit groups and their total deposit sums but only for the wizards who have their magic wands crafted by Ollivander family. 
 -- Filter total deposit amounts lower than 150000. Order by total deposit amount in descending order.
 
